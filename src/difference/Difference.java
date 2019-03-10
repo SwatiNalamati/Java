7691beed242814c22d8b518ab81cac2f45964c4d
SwatiNalamati <47767952+SwatiNalamati@users.noreply.github.com>
2019-03-10 07:16:49 +0530

    Update Difference.java

--- src/difference/Difference.java
+++ src/difference/Difference.java
@@ -3,36 +3,41 @@ package difference;
 import java.util.Scanner;
 
 public class Difference {
+	static Scanner s = new Scanner(System.in);
 
-	public static void main(String[] args) {
-
-		Scanner s = new Scanner(System.in);
+	static int difference(int diff) {
 
 		System.out.println("Enter number");
-
 		int i = s.nextInt();
 
 		if (i <= 21) {
 
-			int difference = 21 - i;
+			diff = 21 - i;
 
-			System.out.println(difference);
+			System.out.println(diff);
 
 		}
 
 		else if (i > 21) {
 
-			int difference = 21 - i;
+			diff = 21 - i;
 			// int positive = Math.abs(difference);
 
-			int positive = difference * -1;
+			int positive = diff * -1;
 			// negative*negative is positive
 			int result = positive * 2;
 
-			System.out.println(difference);
-			System.out.println(positive);
+			// System.out.println(diff);
+			// System.out.println(positive);
 			System.out.println(result);
 		}
+		return diff;
+
+	}
+
+	public static void main(String[] args) {
+		difference(0);
+		// Why 0 as parameter in difference method when its called?
 	}
 
 }
