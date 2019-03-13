bd03c71fc880897de754bc56342e86e31d90da49
SwatiNalamati <47767952+SwatiNalamati@users.noreply.github.com>
2019-03-13 09:05:24 +0530

    Create SwitchDays.java

---
+++ src/programsSwitch/SwitchDays.java
@@ -0,0 +1,78 @@
+package programsSwitch;
+
+import java.util.Scanner;
+
+public class SwitchDays {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter month number");
+		int month = s.nextInt();
+
+		switch (month) {
+		case 1:
+
+			System.out.println("31 days");
+			break;
+
+		case 2:
+
+			System.out.println("28 days");
+			break;
+
+		case 3:
+
+			System.out.println("31 days");
+			break;
+
+		case 4:
+
+			System.out.println("30 days");
+			break;
+
+		case 5:
+
+			System.out.println("31 days");
+			break;
+
+		case 6:
+
+			System.out.println("30 days");
+			break;
+
+		case 7:
+
+			System.out.println("31 days");
+			break;
+
+		case 8:
+
+			System.out.println("31 days");
+			break;
+
+		case 9:
+
+			System.out.println("30 days");
+			break;
+
+		case 10:
+
+			System.out.println("31 days");
+			break;
+
+		case 11:
+
+			System.out.println("30 days");
+			break;
+
+		case 12:
+
+			System.out.println("31 days");
+			break;
+
+		}
+
+	}
+
+}
